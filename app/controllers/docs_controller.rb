3b0a0e0082867498e30bde9903b1c13c699075db
jorjik <grandcazino@gmail.com>
2020-05-21 22:31:12 +0300

    Doc app

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,42 @@
+class DocsController < ApplicationController
+  def index
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end  
+
+  def create
+
+  end
+  
+  def edit
+
+  end
+
+  def update
+
+  end
+  
+  def show
+
+  end
+  
+  def destroy
+
+  end
+
+  private
+  
+    def finde_doc
+
+    end
+    
+    def doc_params
+
+    end
+end
